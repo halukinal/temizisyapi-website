b8c6f62bbc7f8b40bba176fa4426337f8f33b7ae
Haluk İnal <inlhalk@gmail.com>
2026-03-20 22:25:07 +0300

    Fix: Hardcode Firebase config for reliability

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,13 @@
 // lib/firebase.ts (Sadece İSTEMCİ tarafında çalışır)
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyD6Jku-93vod3rntWC-mW4D5WIAsW4XexA",
+    authDomain: "temizisyapi-85066.firebaseapp.com",
+    projectId: "temizisyapi-85066",
+    storageBucket: "temizisyapi-85066.appspot.com",
+    messagingSenderId: "140245080521",
+    appId: "1:140245080521:web:6982c225ae7dede0aa3018",
+    measurementId: "G-SMXJ19244X"
 };
 
 let app: any = null;
